f5c78ff4a75b584a3f05622b405ef4e2fe48a82c
Максим <avtuhmaxim@yandex.by>
2024-05-18 00:10:25 +0300

    Коммит 7

--- src/scripts/choices.js
+++ src/scripts/choices.js
@@ -3,6 +3,19 @@ import {debounce} from './debounce';
 export const initChoices = () => {
   const choices = document.querySelectorAll('.choices');
 
+  const closeAllChoices = ({target}) => {
+    let clickInside = target.closest('.choices');
+    if (!clickInside) {
+      choices.forEach((choice) => {
+        choice
+        .querySelector('.choices__box')
+        .classList.remove('choices__box_open');
+      })
+      document.removeEventListener('click', closeAllChoices);
+
+    }
+  }
+
   const adjustElementPosition = (element, count = 0) => {
     const rect = element.getBoundingClientRect();
     const viewportWidth = window.innerWidth;
@@ -48,11 +61,18 @@ export const initChoices = () => {
         }
       });
 
+      if (box.classList.contains('choices__box_open')) {
+        document.addEventListener('click', closeAllChoices);
+      }
+      else {
+        document.removeEventListener('click', closeAllChoices);
+      }
+
       adjustElementPosition(box);
     });
   
     window.addEventListener('resize ', debounce(() => {
       adjustElementPosition(box);
     }));
-  });
-};
+});
+}
